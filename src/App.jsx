270d6dd6bccada7dd4daa87c5e63a9ee8b8f835f
vfurkanguner <vfurkanguner@gmail.com>
2022-11-08 05:45:24 +0300

    style : create styled Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="relative flex flex-col">
-      <main className=" md:fixed left-0 top-0 overflow-y-auto bottom-0 bg-shadow md:w-1/2 p-2">
-        main
+      <main className="md:fixed left-0 top-0 overflow-y-auto bottom-0 bg-shadow md:w-1/2 p-2">        
+      <Input label="Name" type="text" name="name" />
       </main>
 
-      <aside className="md:fixed right-0 top-0 bottom-0 md:w-1/2 p-2">ben</aside>
+
+      <aside className="md:fixed right-0 top-0 bottom-0 md:w-1/2 p-2 bg-slate-400">ben</aside>
     </div>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Input({
+  label = "",
+  type,
+  name,
+  value,
+  onChange,
+  description = "",
+}) {
+  return (
+    <div className="flex flex-col space-y-1">
+      <label className="text-sm font-medium text-zinc-900">{label}</label>
+      <input
+        type="text"
+        className="border rounded-lg px-4 py-2 shadow focus:outline-none focus:ring-1 focus:ring-indigo-400"
+      />
+      <span className="text-xs font-light text-slate-400">{description}</span>
+    </div>
+  );
+}
